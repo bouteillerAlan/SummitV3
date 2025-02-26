ef6fe5659a9d989a9909d6e6370cad76d0034c82
Bouteiller [a2n] Alan <a2n.dev@pm.me>
2025-02-26 16:01:30 +0100

    🗑️ cleanup migration

--- api/migrations/Version20250226143831.php
+++ api/migrations/Version20250226143831.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250221155622 extends AbstractMigration
+final class Version20250226143831 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,15 +21,18 @@ final class Version20250221155622 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE training (id SERIAL NOT NULL, user_id INT NOT NULL, name VARCHAR(255) NOT NULL, date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_D5128A8F9D86650F ON training (user_id)');
-        $this->addSql('ALTER TABLE training ADD CONSTRAINT FK_D5128A8F9D86650F FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_D5128A8FA76ED395 ON training (user_id)');
+        $this->addSql('CREATE TABLE "user" (id SERIAL NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
+        $this->addSql('ALTER TABLE training ADD CONSTRAINT FK_D5128A8FA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE training DROP CONSTRAINT FK_D5128A8F9D86650F');
+        $this->addSql('ALTER TABLE training DROP CONSTRAINT FK_D5128A8FA76ED395');
         $this->addSql('DROP TABLE training');
+        $this->addSql('DROP TABLE "user"');
     }
 }
